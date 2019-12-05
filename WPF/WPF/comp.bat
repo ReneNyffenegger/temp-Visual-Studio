1140f514a1951c79be779ed7d0cf85ad71508ede
René Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2019-12-05 17:01:25 +0100

    n/a

---
+++ WPF/WPF/comp.bat
@@ -0,0 +1,50 @@
+"C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\MSBuild\Current\Bin\Roslyn\csc.exe" ^
+  /noconfig ^
+  /nowarn:1701,1702,2008 ^
+  /nostdlib+ ^
+  /platform:anycpu32bitpreferred ^
+  /errorreport:prompt ^
+  /warn:4 ^
+  /define:DEBUG;TRACE ^
+  /errorendlocation ^
+  /preferreduilang:en-US ^
+  /highentropyva+ ^
+  /reference:"C:\Program Files (x86)\Reference Assemblies\Microsoft\Framework\.NETFramework\v4.7.2\Microsoft.CSharp.dll"  ^
+  /reference:"C:\Program Files (x86)\Reference Assemblies\Microsoft\Framework\.NETFramework\v4.7.2\mscorlib.dll" ^
+  /reference:"C:\Program Files (x86)\Reference Assemblies\Microsoft\Framework\.NETFramework\v4.7.2\PresentationCore.dll" ^
+  /reference:"C:\Program Files (x86)\Reference Assemblies\Microsoft\Framework\.NETFramework\v4.7.2\PresentationFramework.dll" ^
+  /reference:"C:\Program Files (x86)\Reference Assemblies\Microsoft\Framework\.NETFramework\v4.7.2\System.Core.dll" ^
+  /reference:"C:\Program Files (x86)\Reference Assemblies\Microsoft\Framework\.NETFramework\v4.7.2\System.Data.DataSetExtensions.dll" ^
+  /reference:"C:\Program Files (x86)\Reference Assemblies\Microsoft\Framework\.NETFramework\v4.7.2\System.Data.dll" ^
+  /reference:"C:\Program Files (x86)\Reference Assemblies\Microsoft\Framework\.NETFramework\v4.7.2\System.dll" ^
+  /reference:"C:\Program Files (x86)\Reference Assemblies\Microsoft\Framework\.NETFramework\v4.7.2\System.Net.Http.dll" ^
+  /reference:"C:\Program Files (x86)\Reference Assemblies\Microsoft\Framework\.NETFramework\v4.7.2\System.Xaml.dll" ^
+  /reference:"C:\Program Files (x86)\Reference Assemblies\Microsoft\Framework\.NETFramework\v4.7.2\System.Xml.dll" ^
+  /reference:"C:\Program Files (x86)\Reference Assemblies\Microsoft\Framework\.NETFramework\v4.7.2\System.Xml.Linq.dll" ^
+  /reference:"C:\Program Files (x86)\Reference Assemblies\Microsoft\Framework\.NETFramework\v4.7.2\WindowsBase.dll" ^
+  /debug+ ^
+  /debug:full ^
+  /filealign:512 ^
+  /optimize- ^
+  /out:WPF.exe ^
+  /ruleset:"C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\Team Tools\Static Analysis Tools\\Rule Sets\MinimumRecommendedRules.ruleset" ^
+  /subsystemversion:6.00 ^
+  /resource:obj\Debug\WPF.g.resources ^
+  /resource:obj\Debug\WPF.Properties.Resources.resources ^
+  /target:winexe ^
+  /utf8output ^
+  /deterministic+ ^
+  /langversion:7.3 ^
+  Report.xaml.cs ^
+  App.xaml.cs ^
+  MainWindow.xaml.cs ^
+  PageHome.xaml.cs ^
+  Properties\AssemblyInfo.cs ^
+  Properties\Resources.Designer.cs ^
+  Properties\Settings.Designer.cs ^
+  C:\Users\OMIS.Rene\github\temp\Visual-Studio\WPF\WPF\obj\Debug\MainWindow.g.cs ^
+  C:\Users\OMIS.Rene\github\temp\Visual-Studio\WPF\WPF\obj\Debug\PageHome.g.cs ^
+  C:\Users\OMIS.Rene\github\temp\Visual-Studio\WPF\WPF\obj\Debug\Report.g.cs ^
+  C:\Users\OMIS.Rene\github\temp\Visual-Studio\WPF\WPF\obj\Debug\App.g.cs ^
+  "C:\Users\OMIS.Rene\AppData\Local\Temp\.NETFramework,Version=v4.7.2.AssemblyAttributes.cs"
+
