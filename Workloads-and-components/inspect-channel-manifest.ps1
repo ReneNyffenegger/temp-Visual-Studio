5bef3b5cf5c54712140e4d9b3e8d888e1e93dbf5
René Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2025-07-31 13:53:00 +0200

    .

--- Workloads-and-components/inspect-channel-manifest.ps1
+++ Workloads-and-components/inspect-channel-manifest.ps1
@@ -1,7 +1,13 @@
 #
+# Idea from
+#    https://gist.github.com/mmozeiko/7f3162ec2988e81e56d5c4e22cde9977
+#
+#
 # VS 17 = Visual Studio 2022
 #
-invoke-webrequest https://aka.ms/vs/17/release/channel -outFile VisualStudio.17.Release.chman
+# invoke-webrequest https://aka.ms/vs/17/release/channel -outFile VisualStudio.17.Release.chman
+  invoke-webrequest https://aka.ms/vs/17/pre/channel     -outFile VisualStudio.17.Preview.chman
+
 
 cat VisualStudio.17.Release.chman | jq keys
 #
@@ -53,5 +59,40 @@ cat VisualStudio.17.Release.chman | jq  .channelItems[].id
 #  "VisualStudio.17.Release"
 #  "VisualStudio.17.Release.Bootstrappers.Setup"
 #
+
+$vsPayloadUrl = $(cat VisualStudio.17.Release.chman | jq -r '.channelItems[]  | select(.id == ""Microsoft.VisualStudio.Manifests.VisualStudio""        ).payloads[0].url')
+write-host $vsPayloadUrl
+
+
+
+# $vsPayload = (invoke-webrequest $vsPayloadUrl).content
+# write-host $vsPayload
+$progressPreference = 'SilentlyContinue'
+                invoke-webRequest $vsPayloadUrl -outFile VisualStudio.vsman
+
+# cat VisualStudio.vsman | jq .packages
+  cat VisualStudio.vsman | jq .packages[].id
+
+$vs = get-content VisualStudio.vsman  -raw | convertFrom-json
+
+$packages = @{}
+
+foreach ($package in $vs.packages) {
+   if (-not $packages.ContainsKey($package.id)) {
+       $packages[$package.id] = @()
+   }
+   $packages[$package.id] += $package
+}
+
+foreach ($p in $packages.keys) {
+   write-host "$p - $($packages[$p].length)"
+}
+
+
+
+return
+
+#  Alternatively, if using preview url:
+# cat VisualStudio.17.Release.chman | jq -r '.channelItems[]  | select(.id == ""Microsoft.VisualStudio.Manifests.VisualStudioPreview"" ).payloads[0].url'
 
 cat VisualStudio.17.Release.chman | jq  '.channelItems[]  | select(.id == ""Microsoft.VisualStudio.Product.Community"" and .productArch == ""x64"" )'
